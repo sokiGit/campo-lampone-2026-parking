f9912492ebf3769b54a9401af6c9ccab639e1f0d
FoxyCzech7 <foxyczech8@gmail.com>
2026-08-17 12:22:53 +0200

    modra

--- parkoviste.py
+++ parkoviste.py
@@ -1,43 +1,37 @@
-
-from collections import deque
-
 import cv2
 import numpy as np
-import requests
 
-
-
-
+import image_tools
 
 cap = cv2.VideoCapture("http://gateson.lan/stream")
 
-while(True):
-    _, image = cap.read()
-
-
-    cv2.imshow("Output", image)
-
-    # cv2.imshow("Output", true_red_only)
-
-
-    _, image1 = cv2.threshold(image[:,:,0],127,253,cv2.THRESH_BINARY)
-    _, image2 = cv2.threshold(image[:,:,1],127,253,cv2.THRESH_BINARY)
-    _, image3 = cv2.threshold(image[:,:,2],127,253,cv2.THRESH_BINARY)
-
-    image_return = cv2.bitwise_or(image2,image3)
-    image_not  = cv2.bitwise_not(image_return)
-    image_end = cv2.bitwise_and(image_return, image1)
-
-
-    print (image.shape)
-
-    cv2.imshow("output", image_return)
-
-    if cv2.waitKey(1) == ord('q'):
+while True:
+    ret, image = cap.read()
+    if not ret:
         break
 
-    from collections import deque
-    dq = deque([1, 2])
+    # 1. Převod obrázku z BGR do HSV
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    print(dq[0])
-    print(dq[-1])
+    # 2. Definoování rozsahů pro červenou barvu v HSV
+    # Červená barva v HSV přetéká přes hranici 0/180°, proto potřebujeme dva rozsahy
+    # Hodnota S (saturace) nastavená na min. 100 spolehlivě odfiltruje bílou (bílá má S blízko 0)
+    lower_red1 = np.array([40, 100, 100])
+    upper_red1 = np.array([60, 255, 255])
+
+    # 3. Vytvoření masek
+    mask1 = image_tools.extract_color_mask_min(hsv, 100, 140)
+
+    # 4. Spojení obou masek
+    # Výsledná maska obsahuje 255 (bílou) na místech červené a 0 (černou) jinde
+
+
+    # Zobrazení
+    cv2.imshow("Původní obraz", mask1)
+    #cv2.imshow("Červená jako bílá", red_as_white)
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
