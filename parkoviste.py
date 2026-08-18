b9f3bb40464e4cc0cf8f3ae7825c8f7ac8caa1fd
FoxyCzech7 <foxyczech8@gmail.com>
2026-08-18 08:55:46 +0200

    zavora

--- parkoviste.py
+++ parkoviste.py
@@ -4,6 +4,7 @@ import urllib.request
 import cv2
 import easyocr
 import numpy as np
+import requests
 
 import image_tools
 
@@ -31,11 +32,9 @@ while True:
     mask_blue = image_tools.extract_color_mask_min(hsv, 100, 140)
     blue_pixels = cv2.countNonZero(mask_blue)
 
-    # 1. Kontrola barvy probíhá JEN pokud ještě neodpočítáváme čas po závoře
     if konecny_cas == 0:
         if blue_pixels > MIN_PIXELS:
             if not led_sviti:
-                # FÁZE A: První modrá LED -> Stáhnutí obrázku a OCR
                 if not ceka_na_zelenou and not ceka_na_druhou_modrou:
                     print("Modrá LED rozsvícena! Stahuji obrázek...")
 
@@ -61,12 +60,15 @@ while True:
                     except Exception as e:
                         print(f"Chyba při stahování nebo OCR: {e}")
 
-                # FÁZE C: Druhá modrá LED -> Ignorování dalších barev, závora a start časovače
                 elif ceka_na_druhou_modrou:
                     print("Druhá modrá LED detekována! Ignoruji další barvy.")
-                    print("Otevírám závoru...")
-                    time.sleep(5)  # Závora otevřená 5 sekund
+                    print("Otevírám závoru.")
+                    urllib.request.urlopen("http://gateson.lan/gate_in?open")
+
+                    time.sleep(5)
+
                     print("Zavírám závoru.")
+                    urllib.request.urlopen("http://gateson.lan/gate_in?close")
 
                     # Odpočet zbývajících 10 s (celkem 15 s od otevření závory)
                     konecny_cas = time.time() + 10
@@ -76,7 +78,6 @@ while True:
         else:
             led_sviti = False
 
-        # FÁZE B: Čekání na zelenou LED
         if ceka_na_zelenou:
             mask_green = image_tools.extract_color_mask_min(hsv, 35, 85)
             green_pixels = cv2.countNonZero(mask_green)
@@ -86,7 +87,6 @@ while True:
                 ceka_na_zelenou = False
                 ceka_na_druhou_modrou = True
 
-    # 2. Odpočet času do odjezdu (beží na pozadí neovlivněn obrazem)
     if konecny_cas > 0:
         zbyvajici_cas = konecny_cas - time.time()
 
